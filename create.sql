f300de0cf2b76b2a1364dae2f1a15b0fdd25cb67
MtZero <zhonglsh7@mail2.sysu.edu.cn>
2017-12-18 15:29:29 +0800

    Update create.sql

--- create.sql
+++ create.sql
@@ -3,18 +3,16 @@ CREATE DATABASE `tieba` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `tieba`;
 
 CREATE TABLE `users` (
-  `uid`      INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `username` CHAR(30)         NOT NULL,
-  `password` CHAR(45)         NOT NULL,
-  `role`     INT(11)          NOT NULL DEFAULT '0',
-  `avatar`   VARCHAR(100)              DEFAULT NULL,
-  PRIMARY KEY (`uid`, `username`),
+  `uid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` char(30) NOT NULL,
+  `password` char(45) NOT NULL,
+  `role` int(11) NOT NULL DEFAULT '0',
+  `avatar` varchar(200) NOT NULL DEFAULT 'default-avatar.jpg',
+  PRIMARY KEY (`uid`,`username`),
   UNIQUE KEY `uid_UNIQUE` (`uid`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-)
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 6
-  DEFAULT CHARSET = utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+
 
 
 /* The admin's password should be set manually */
@@ -22,71 +20,55 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`username`, `password`, `role`) VALUES ('admin', '45d76d8f3ce00ecd799b03a73bf825c8', 99);
 
 CREATE TABLE `posts` (
-  `pid`        INT(11)          NOT NULL AUTO_INCREMENT,
-  `uid`        INT(10) UNSIGNED NOT NULL,
-  `p_title`    VARCHAR(100)              DEFAULT NULL,
-  `p_content`  VARCHAR(10000)            DEFAULT NULL,
-  `p_datetime` DATETIME         NOT NULL,
-  `p_floor`    INT(11)          NOT NULL DEFAULT '1',
-  `r_uid`      INT(10) UNSIGNED          DEFAULT NULL,
-  `r_datetime` DATETIME                  DEFAULT NULL,
-  `state`      INT(11)          NOT NULL DEFAULT '0',
+  `pid` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(10) unsigned NOT NULL,
+  `p_title` varchar(100) DEFAULT NULL,
+  `p_content` varchar(10000) DEFAULT NULL,
+  `p_datetime` datetime NOT NULL,
+  `p_floor` int(11) NOT NULL DEFAULT '1',
+  `r_uid` int(10) unsigned DEFAULT NULL,
+  `r_datetime` datetime DEFAULT NULL,
+  `state` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`pid`),
   UNIQUE KEY `pid_UNIQUE` (`pid`),
-  KEY `uid_idx` (`uid`, `r_uid`),
+  KEY `uid_idx` (`uid`,`r_uid`),
   KEY `posts2_idx` (`r_uid`),
-  KEY `datetime` (`r_datetime`, `p_datetime`),
-  CONSTRAINT `posts2` FOREIGN KEY (`r_uid`) REFERENCES `users` (`uid`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-)
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 10
-  DEFAULT CHARSET = utf8mb4;
-
-CREATE DEFINER =`root`@`localhost` TRIGGER `tieba`.`posts_BEFORE_INSERT`
-  BEFORE INSERT
-  ON `posts`
-  FOR EACH ROW
-  BEGIN
-    SET new.r_datetime = new.p_datetime;
-  END;
+  KEY `datetime` (`r_datetime`,`p_datetime`),
+  CONSTRAINT `posts2` FOREIGN KEY (`r_uid`) REFERENCES `users` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `comments` (
-  `cid`        INT(11)          NOT NULL AUTO_INCREMENT,
-  `pid`        INT(11)          NOT NULL,
-  `uid`        INT(10) UNSIGNED NOT NULL,
-  `c_floor`    INT(11)          NOT NULL DEFAULT '0',
-  `r_floor`    INT(11)                   DEFAULT NULL,
-  `c_content`  VARCHAR(10000)            DEFAULT NULL,
-  `c_datetime` DATETIME         NOT NULL,
-  `state`      INT(11)          NOT NULL DEFAULT '0',
+  `cid` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) NOT NULL,
+  `uid` int(10) unsigned NOT NULL,
+  `c_floor` int(11) NOT NULL DEFAULT '0',
+  `r_floor` int(11) DEFAULT NULL,
+  `c_content` varchar(10000) DEFAULT NULL,
+  `c_datetime` datetime NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`cid`),
   UNIQUE KEY `cid_UNIQUE` (`cid`),
-  KEY `uid_idx` (`uid`),
   KEY `pid_idx` (`pid`),
-  CONSTRAINT `comments` FOREIGN KEY (`uid`) REFERENCES `posts` (`uid`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-)
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 6
-  DEFAULT CHARSET = utf8mb4;
+  KEY `comments_idx` (`uid`),
+  CONSTRAINT `comments` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4;
 
-CREATE DEFINER =`root`@`localhost` TRIGGER `tieba`.`comments_BEFORE_INSERT`
-  BEFORE INSERT
-  ON `comments`
-  FOR EACH ROW
-  BEGIN
-    DECLARE sum_floor INT;
-    SET sum_floor = (SELECT `p_floor`
-                     FROM `posts`
-                     WHERE `pid` = new.pid FOR UPDATE);
+DELIMITER $$
+USE `tieba`$$
+CREATE TRIGGER `tieba`.`posts_BEFORE_INSERT` BEFORE INSERT ON `posts` FOR EACH ROW
+BEGIN
+	SET new.r_datetime = new.p_datetime;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+USE `tieba`$$
+CREATE TRIGGER `tieba`.`comments_BEFORE_INSERT` BEFORE INSERT ON `comments` FOR EACH ROW
+BEGIN
+	DECLARE sum_floor int;
+    SET sum_floor = (SELECT `p_floor` FROM `posts` WHERE `pid` = new.pid FOR UPDATE);
     SET new.c_floor = sum_floor;
-    UPDATE `posts`
-    SET `p_floor` = `p_floor` + 1
-    WHERE `pid` = new.pid;
-    UPDATE `posts`
-    SET `r_datetime` = new.c_datetime
-    WHERE `pid` = new.pid;
-  END;
+    UPDATE `posts` SET `p_floor` = `p_floor` + 1 WHERE `pid` = new.pid;
+    UPDATE `posts` SET `r_datetime` = new.c_datetime WHERE `pid` = new.pid;
+END$$
+DELIMITER ;
